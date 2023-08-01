77cec9979feb8029c818b7431959bbf43c4dc8f2
NAGAVENIGOWDA <nagavenigowda017@gmail.com>
2023-08-01 12:15:25 +0530

    initial commit

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Article.css"
+
+function Article() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Article
---
+++ src/containers/blog/Blog.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Blog.css"
+
+function Blog() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Blog
